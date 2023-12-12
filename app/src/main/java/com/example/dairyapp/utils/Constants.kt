88d68f4629f803db70a77fbefad52ad6f5383e85
Jerome Raymond <jeromeraymond@JeromesacStudio.lan>
2023-12-11 21:20:04 -0600

    first commit

---
+++ app/src/main/java/com/example/dairyapp/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.dairyapp.utils
+
+object Constants {
+    const val APP_ID = "application-0-kpfqy"
+    const val CLIENT_ID = "270104702978-eisgskmrk8mkhubqbu310rds2svma3uc.apps.googleusercontent.com"
+}
